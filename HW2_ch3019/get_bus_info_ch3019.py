4e8d87a0ab8aaccb6db94fafc0c70cf9ee4cb036
nnhoucheng <nnhoucheng@gmail.com>
2016-09-21 17:01:44 -0400

    Update get_bus_info_ch3019.py

--- HW2_ch3019/get_bus_info_ch3019.py
+++ HW2_ch3019/get_bus_info_ch3019.py
@@ -12,13 +12,14 @@ apikey = sys.argv[1]
 bus = sys.argv[2]
 fout = open(sys.argv[3], "w")
 
-url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s" %(apikey, bus)
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"\
+%(apikey, bus)
 
 response = urllib2.urlopen(url)
-data = response.read().decode("utf-8")
+d = response.read().decode("utf-8")
 
 #use the json.loads method to obtain a dictionary representation of the responose string 
-dataDict = json.loads(data)
+dataDict = json.loads(d)
 
 data = dataDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
 bus_len = len(data)
@@ -27,12 +28,13 @@ fout.write("Latitude,Longitude,Stop Name,Stop Status\n")
 for i in range(bus_len):
     lo = data[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
     la = data[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
-    if data[i]['MonitoredVehicleJourney']['MonitoredCall'] == {}:
-        sn = "N/A"
-        ss = "N/A"
+    if data[i]['MonitoredVehicleJourney']['OnwardCalls'] == {}:
+        sn = 'N/A'
+        ss = 'N/A'
     else:
-        sn = data[i]['MonitoredVehicleJourney']['MonitoredCall']['StopPointName']    
-        ss = data[i]['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['PresentableDistance']
+        sn = data[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
+        ss = data[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]\
+        ['Extensions']['Distances']['PresentableDistance']
     fout.write("%f,%f,%s,%s\n" % (la, lo, sn, ss))
     
 fout.close()
